5f4180fa3c4ddeb41f31e4167a2d3ab681339fd4
psbreeze <psbreeze.and@gmail.com>
2018-12-08 04:23:05 +0000

    deleted:    Multilayer_Perceptron_Model.ipynb 	deleted:    Multilayer_Perceptron_Model.py 	deleted:    helloworld.py 	deleted:    pima-indians-diabetes.csv 	week1/ 	week2/

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
-
-config = tf.ConfigProto()
-config.gpu_options.visible_device_list='2'
-config.gpu_options.per_process_gpu_memory_fraction = 0.25
-set_session(tf.Session(config=config))
-
-print("hello world!")
